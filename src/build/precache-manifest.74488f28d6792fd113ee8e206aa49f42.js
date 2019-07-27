19a9dfece1580518858150ba08c7bab7e524002c
Yomudogly <khalnepesov@gmail.com>
2019-07-27 09:40:22 -0400

    First commit

---
+++ src/build/precache-manifest.74488f28d6792fd113ee8e206aa49f42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2056662e084f4533de9449a33f8dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a13a6c5ae1518d1d2742",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "a13a6c5ae1518d1d2742",
+    "url": "/static/js/main.ee381fa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
